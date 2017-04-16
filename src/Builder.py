d73f0a16e9dbd81f701a7f95b105d5c71d115911
Hugo Lefeuvre <hle@debian.org>
2017-04-16 16:27:17 +0200

    Avoid code duplication in Builder

--- src/Builder.py
+++ src/Builder.py
@@ -4,63 +4,56 @@ import os
 from jinja2 import Template
 from src import FileParser
 
+def _createIfNonExistent(directory):
+    """ Create passed directory if it doesn't exists already. """
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def _write(directory, string):
+    """ Write passed string to passed directory. """
+
+    with open(directory, 'w') as stream:
+        stream.write(string)
+
+def _formatArgsAndRender(args, page, template):
+    """ Add required entries to rendering args and return rendered template. """
+
+    renderingArgs = args
+    renderingArgs["page"] = page
+    renderingArgs["content"] = page["body"]
+    return template.render(args)
+
 def buildStatics(args):
     """ Build static pages. """
 
     builddir = args["site"]["build-directory"]
-
-    if not os.path.exists(builddir):
-        os.makedirs(builddir)
+    _createIfNonExistent(builddir)
 
     for static in args["statics"]:
-        # Render static page
-        # TODO static pages should also allow layouts !
         html = Template(static["body"]).render(args)
-
-        # Save static pgae
-        with open(builddir + "/" + static["file-name"], 'w') as stream:
-            stream.write(html)
+        _write(builddir + "/" + static["file-name"], html)
 
 def buildPosts(args, jinjaEnv):
     """ Build posts. """
 
-    postBuilddir = args["site"]["build-directory"] + "/posts/"
+    builddir = args["site"]["build-directory"] + "/posts/"
+    _createIfNonExistent(builddir)
 
-    if not os.path.exists(postBuilddir):
-        os.makedirs(postBuilddir)
-
-    # Pre-render post template.
     template = jinjaEnv.get_template("templates/posts.html")
 
     for post in args["posts"]:
-        # Render post
-        renderingArgs = args
-        renderingArgs["page"] = post
-        renderingArgs["content"] = post["body"]
-        html = template.render(args)
-
-        # Save post
-        with open(postBuilddir + post["title-slugified"], 'w') as stream:
-            stream.write(html)
+        html = _formatArgsAndRender(args, post, template)
+        _write(builddir + post["title-slugified"], html)
 
 def buildPages(args, jinjaEnv):
     """ Build pages. """
 
     builddir = args["site"]["build-directory"]
+    _createIfNonExistent(builddir)
 
-    if not os.path.exists(builddir):
-        os.makedirs(builddir)
-
-    # Pre-render post template.
     template = jinjaEnv.get_template("templates/pages.html")
 
     for page in args["pages"]:
-        # Render page
-        renderingArgs = args
-        renderingArgs["page"] = page
-        renderingArgs["content"] = page["body"]
-        html = template.render(args)
-
-        # Save page
-        with open(builddir + page["permalink"], 'w') as stream:
-            stream.write(html)
+        html = _formatArgsAndRender(args, page, template)
+        _write(builddir + page["permalink"], html)
