ca87c2b23f62158a5d6b6473d3fd588e96d4caa8
Hugo Lefeuvre <hle@debian.org>
2017-04-16 15:31:12 +0200

    Cleanup FileParser and Loader. Improve documentation.

--- src/FileParser.py
+++ src/FileParser.py
@@ -4,27 +4,44 @@ import os
 
 def getParsed(rawFile):
     """ Return a dictionnary containing the body part and header part of passed
-    file. None if passed file is bad formatted. """
+    file. If there's no header part, an empty string is returned as header part.
+    Same for body. If file is bad formatted, None is returned."""
 
-    cnt = 0
     result = {}
-    passedLines = ""
-    firstLine = False
-    for line in rawFile.splitlines():
-        if(firstLine):
-            firstLine = False
-            if(line == ""):
-                continue
-        if(line == '---' and cnt != 3):
-            cnt+=1
-            if(cnt == 2):
-                result["header"] = passedLines
-                passedLines = ""
-                firstLine = True
-                cnt+=1
+    parsedLines = ""
+    splittedFile = rawFile.splitlines()
+
+    firstBodyLine = False
+    headerPart = False
+
+    if(splittedFile.pop(0) == "---"):
+        # First line of passed file declares a header.
+        headerPart = True
+    else:
+        result["header"] = []
+        firstBodyLine = True
+
+    for line in splittedFile:
+        if(firstBodyLine and line == ""):
+            # First line of the body is empty. Ignore it.
+            firstBodyLine = False
             continue
-        passedLines+=line+"\n"
-    if(cnt != 3):
+
+        if(line == "---" and headerPart):
+            # Header's end. Save content of header in result["header"] and
+            # reinitialize parsedLines for body
+            headerPart = False
+            firstBodyLine = True
+            result["header"] = parsedLines
+            parsedLines = ""
+            continue
+
+        parsedLines += (line + "\n")
+
+    if(headerPart and "header" not in result.keys()):
+        # Header was declared, but never closed. Something's wrong, sorry !
         return None
-    result["body"] = passedLines
+
+    result["body"] = parsedLines
+
     return result
