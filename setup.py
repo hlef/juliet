cf93db3429ce567acba0c47b9e83c9622e850d94
Hugo Lefeuvre <hle@debian.org>
2017-04-22 17:59:03 +0200

    Update package structure.
    Rename src/ to juliet/, add test directory with some first draft of
    tests and add setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='juliet',
+      version='0.1',
+      description='The lightweight static website generator',
+      url='http://github.com/hlef/juliet',
+      author='Hugo Lefeuvre',
+      author_email='hle@owl.eu.com',
+      license='MIT',
+      packages=['juliet'],
+      zip_safe=False)
