52650d1388c82ed095b04b6bb489a1fd3821ccc0
Hugo Lefeuvre <hle@debian.org>
2017-04-24 22:06:48 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python3
+
 from setuptools import setup
 
 setup(name='juliet',
-      version='0.1~alpha',
+      version='0.1-alpha.1',
       description='The lightweight static website generator',
       url='http://github.com/hlef/juliet',
       author='Hugo Lefeuvre',
       author_email='hle@owl.eu.com',
       license='MIT',
       packages=['juliet'],
+      scripts=['juliet.py'],
       zip_safe=False,
       test_suite='nose.collector',
-      tests_require=['nose'],)
+      tests_require=['nose'])
