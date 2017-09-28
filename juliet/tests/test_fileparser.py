d2ee5ca6db3ced6e6df6dccefad6a45e4be45a48
Hugo Lefeuvre <hle@debian.org>
2017-09-28 15:21:05 +0200

    Write three new tests. Cleanup in tests/.
    Cover tag/block escaping with test_escape_baseurl_tags and
    test_escape_pygments_blocks.
    
    Headers should not be absent ! Cover it with
    test_get_header_limit_with_invalid_files and fix
    test_get_header_limit accordingly.
    
    Save baseurl as class attribute. Modify setUp method accordingly.
    
    Various documentation updates and minor cleanups.

--- juliet/tests/test_fileparser.py
+++ juliet/tests/test_fileparser.py
@@ -4,44 +4,57 @@ from juliet.pageprocessor import PageProcessor
 class fileParserTest(unittest.TestCase):
 
     FILENAME = "filename"
+    baseurl = "/base/url"
 
     def setUp(self):
-        self.processor = PageProcessor("/base/url")
+        self.processor = PageProcessor(self.baseurl)
 
-    def test_get_header_limit1(self):
+    def test_get_header_limit(self):
         """ Make sure that _get_header_limit() is working well with simple files. """
 
         splittedFile1 = ["---", "---", "body"]
-        splittedFile2 = ["body"]
-        splittedFile3 = ["---", "key:value", "---", "body"]
+        splittedFile2 = ["---", "key:value", "---", "body"]
+        splittedFile2 = ["---", "key:value", "some42:42", "---"]
 
         self.assertEqual(1, self.processor._get_header_limit(splittedFile1))
-        self.assertEqual(-1, self.processor._get_header_limit(splittedFile2))
-        self.assertEqual(2, self.processor._get_header_limit(splittedFile3))
+        self.assertEqual(3, self.processor._get_header_limit(splittedFile2))
 
-    def test_check_header1(self):
-        """ Make sure that _check_header() is working properly when passing
+    def test_get_header_limit_with_invalid_files(self):
+        """ Make sure that _get_header_limit() is working well with invalid files. """
+
+        no_header = ["body"]
+        self.assertRaises(ValueError, self.processor._get_header_limit, no_header)
+
+        only_one_sep1 = ["---"]
+        self.assertRaises(ValueError, self.processor._get_header_limit, only_one_sep1)
+
+        only_one_sep2 = ["", "---"]
+        self.assertRaises(ValueError, self.processor._get_header_limit, only_one_sep2)
+
+        only_one_sep3 = ["---", "", "test"]
+        self.assertRaises(ValueError, self.processor._get_header_limit, only_one_sep3)
+
+    def test_check_header_content1(self):
+        """ Make sure that _check_header_content() is working properly when passing
         invalid headers."""
 
         invalid_header = {"body": "value"}
 
-        self.assertRaises(ValueError, self.processor._check_header, invalid_header)
+        self.assertRaises(ValueError, self.processor._check_header_content, invalid_header)
 
-    def test_check_header2(self):
-        """ Make sure that _check_header() is working properly when passing
+    def test_check_header_content2(self):
+        """ Make sure that _check_header_content() is working properly when passing
         valid headers."""
 
         valid_header = {"key": "value", "key2": "value2", "foo": "bar"}
 
-        self.processor._check_header(valid_header)
+        self.processor._check_header_content(valid_header)
 
-    def test_process_header1(self):
+    def test_process_header(self):
         """ Make sure that the _process_header() method is working well with
         simple headers."""
 
-        header = """key: value
-key2: value2
-foo: bar"""
+        header = """key: value\nkey2: value2\nfoo: bar"""
 
         result = {"key": "value", "key2": "value2", "foo": "bar"}
 
@@ -51,13 +64,7 @@ foo: bar"""
         """ Make sure that process() returns the excepted result when passing
         simple, valid files."""
 
-        validFile = """---
-key: value
-whatever: anothervalue
-22i: valuewithnumbers5
----
-
-body"""
+        validFile = """---\nkey: value\nwhatever: anothervalue\n22i: valuewithnumbers5\n---\n\nbody"""
 
         result = {"key": "value", "whatever": "anothervalue", "22i": "valuewithnumbers5",
                   "body": "<p>body</p>", 'file-name': self.FILENAME}
@@ -68,68 +75,70 @@ body"""
         """ Make sure that process() returns the excepted result when passing
         simple, valid files with empty header and body."""
 
-        validFile1 = """---
----
-
-body"""
-
-        validFile2 = """body"""
-
+        validFile1 = """---\n---\n\nbody"""
         result1 = {"body": "<p>body</p>", 'file-name': self.FILENAME}
 
-        validFile3 = """---
-
----"""
-
-        result3 = {"body": "", 'file-name': self.FILENAME}
-
-        validFile4 = """"""
-
-        result4 = {"body": "", 'file-name': self.FILENAME}
+        validFile2 = """---\n\n---"""
+        result2 = {"body": "", 'file-name': self.FILENAME}
 
         self.assertEqual(result1, self.processor.process(validFile1, self.FILENAME))
-        self.assertEqual(result1, self.processor.process(validFile2, self.FILENAME))
-        self.assertEqual(result3, self.processor.process(validFile3, self.FILENAME))
-        self.assertEqual(result4, self.processor.process(validFile4, self.FILENAME))
+        self.assertEqual(result2, self.processor.process(validFile2, self.FILENAME))
 
     def test_parsing_invalid_file(self):
         """ Make sure that process() returns None if passed file is invalid."""
 
-        invalidFile = """---
-key: value
-
-body"""
+        invalidFile = """---\nkey: value\nbody"""
 
         self.assertRaises(ValueError, self.processor.process, invalidFile, self.FILENAME)
 
     def test_baseurl_tags_parsing(self):
         """ Make sure that baseurl tags are well parsed. """
 
-        baseurl = "/home/user/test/"
-
-        body_with_baseurl1 = ["{{baseurl}}",
-                              "{{ baseurl}}",
-                              "{{baseurl     }}",
-                              "{{    baseurl}}"]
-
-        result_body_with_baseurl1 = [baseurl, baseurl, baseurl, baseurl]
+        body_with_baseurl1 = ["{{baseurl}}", "{{ baseurl}}", "{{baseurl     }}", "{{    baseurl}}"]
+        result_body_with_baseurl1 = [self.baseurl, self.baseurl, self.baseurl, self.baseurl]
 
         body_with_baseurl2 = ["How, well, here it is: {{ baseurl}}."]
-
-        result_body_with_baseurl2 = ["How, well, here it is: " + baseurl + "."]
+        result_body_with_baseurl2 = ["How, well, here it is: " + self.baseurl + "."]
 
         body_with_baseurl3 = ["several lines", "{{baseurl }}", "because thats fun"]
+        result_body_with_baseurl3 = ["several lines", self.baseurl, "because thats fun"]
 
-        result_body_with_baseurl3 = ["several lines", baseurl, "because thats fun"]
+        self.assertEqual(result_body_with_baseurl1, self.processor._process_baseurl_tags(body_with_baseurl1, self.baseurl))
+        self.assertEqual(result_body_with_baseurl2, self.processor._process_baseurl_tags(body_with_baseurl2, self.baseurl))
+        self.assertEqual(result_body_with_baseurl3, self.processor._process_baseurl_tags(body_with_baseurl3, self.baseurl))
 
-        self.assertEqual(result_body_with_baseurl1, self.processor._process_baseurl_tags(body_with_baseurl1, baseurl))
-        self.assertEqual(result_body_with_baseurl2, self.processor._process_baseurl_tags(body_with_baseurl2, baseurl))
-        self.assertEqual(result_body_with_baseurl3, self.processor._process_baseurl_tags(body_with_baseurl3, baseurl))
+    def test_escape_baseurl_tags(self):
+        """ Make sure that baseurl tags escaping works. """
+
+        body_with_baseurl1 = [r"\{{ baseurl }} = {{ baseurl }}"]
+        result1 = [r"{{ baseurl }} = " + self.baseurl]
+
+        body_with_baseurl2 = [r"\{{ baseurl }}, some text\\{{baseurl}}, and some other TEXT\\\{{baseurl  }}here, and also \\\\{{baseurl}}"]
+        result2 = [r"{{ baseurl }}, some text\{{baseurl}}, and some other TEXT\\{{baseurl  }}here, and also \\\{{baseurl}}"]
+
+        self.assertEqual(result1, self.processor._process_baseurl_tags(body_with_baseurl1, self.baseurl))
+        self.assertEqual(result2, self.processor._process_baseurl_tags(body_with_baseurl2, self.baseurl))
+
+    def test_escape_pygments_blocks(self):
+        """ Make sure that pygments blocks escaping works. """
+
+        body_with_pygments_blocks1 = [r"\{% highlight shell %}\{% endhighlight %}"]
+        result1 = [r"{% highlight shell %}{% endhighlight %}"]
+
+        body_with_pygments_blocks2 = [r"\\{% highlight shell %}\\{% endhighlight %}"]
+        result2 = [r"\{% highlight shell %}\{% endhighlight %}"]
+
+        body_with_pygments_blocks3 = [r"\\\{% highlight shell %}\\\{% endhighlight %}"]
+        result3 = [r"\\{% highlight shell %}\\{% endhighlight %}"]
+
+        self.assertEqual(result1, self.processor._process_pygments(body_with_pygments_blocks1))
+        self.assertEqual(result2, self.processor._process_pygments(body_with_pygments_blocks2))
+        self.assertEqual(result3, self.processor._process_pygments(body_with_pygments_blocks3))
 
     def test_invalid_pygments(self):
         """ Make sure that parsing hangs on invalid pygments blocks."""
 
-        opened_but_never_closed = """---
+        opened_but_never_closed = r"""---
 ---
 
 {% highlight shell %}
