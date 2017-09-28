502f7d8a781c7dc9ce2d307a1772f696e91ab5a9
Hugo Lefeuvre <hle@debian.org>
2017-09-28 16:54:53 +0200

    Release alpha3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='juliet',
-      version='0.1-alpha2',
+      version='0.1-alpha3',
       description='The lightweight static website generator',
       url='http://github.com/hlef/juliet',
       author='Hugo Lefeuvre',
