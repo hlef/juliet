e4bcd38012e7463754fd0d20bf846e1477bf5f79
Hugo Lefeuvre <hle@debian.org>
2018-04-28 15:00:56 -0400

    Stubs for 'juliet init' and 'juliet new' tests

---
+++ juliet/tests/test_init_feature.py
@@ -0,0 +1,15 @@
+import unittest
+
+class initSiteTest(unittest.TestCase):
+
+    def test_without_dir(self):
+        """ Make sure new article files are well generated with minimal set of
+        options. """
+
+        raise NotImplementedError("test not implemented")
+
+    def test_with_dir(self):
+        """ Make sure new article files are well generated when date option is
+        passed. """
+
+        raise NotImplementedError("test not implemented")
---
+++ juliet/tests/test_new_article.py
@@ -0,0 +1,31 @@
+import unittest
+
+class newArticleFileTest(unittest.TestCase):
+
+    def test_without_date(self):
+        """ Make sure new article files are well generated with minimal set of
+        options. """
+
+        raise NotImplementedError("test not implemented")
+
+    def test_with_date(self):
+        """ Make sure new article files are well generated when date option is
+        passed. """
+
+        raise NotImplementedError("test not implemented")
+
+    def test_with_dest_folder(self):
+        """ Make sure new article files are well generated when a destination
+        folder is specified. """
+
+        raise NotImplementedError("test not implemented")
+
+    def test_invalid_date1(self):
+        """ Make sure nothing is done in case date is invalid. """
+
+        raise NotImplementedError("test not implemented")
+
+    def test_invalid_date2(self):
+        """ Make sure nothing is done in case date is invalid. """
+
+        raise NotImplementedError("test not implemented")
